9d653c5f896b013dd742ca89b3a9cb48336e3f23
lonardsteven <113404058+lonardsteven@users.noreply.github.com>
2023-11-26 20:03:35 +0700

    Update the branch with latest project changes

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# Shell scripting untuk build (membangun) dan push image aplikasi Karsajobs frontend / UI
+# untuk assignment Dicoding 2 - Membangun Arsitektur Microservices
+# Nama siswa: Lonard Steven - Jakarta
+			
+# Membangun image docker (local) dari Dockerfile yang telah tersedia dengan nama karsajobs-ui (mengingat script untuk bagian frontend aplikasi)
+# dan dibuatkan untuk versi latest. Image akan menggunakan username lonard2.
+docker build -t lonard2/karsajobs-ui:latest .
+
+# Membuat sebuah environment variable bernama PAT_FE (personal access token) yang berisikan token PAT dari GitHub yang telah diperbolehkan
+# (allowed) akses write & read package serta repository / repo. Perannya ialah untuk mendukung proses login Docker.
+export PAT_FE=
+
+# Login Docker ke container registry ghcr.io (GitHub Packages) dengan username GitHub pribadi dan password account berupa PAT_BE (environment variable)
+# pada line sebelumnya.
+echo $PAT_FE | docker login ghcr.io -u lonard2 --password-stdin
+
+# Dengan tag, nama image Docker dari nama local yakni lonard2/karsajobs-ui:latest menjadi nama yang mengikuti standar GitHub Packages
+# (ghcr.io/NAMESPACE/package_name:tag) yakni ghcr.io/lonard2/karsajobs-ui:latest
+docker tag lonard2/karsajobs-ui:latest ghcr.io/lonard2/karsajobs-ui:latest
+
+# Melakukan push atau upload image ke GitHub Packages melalui GitHub Container Registry (ghcr.io) dengan nama karsajobs-ui
+# untuk versi latest. Username yang digunakan ialah username pribadi (lonard2).
+docker push ghcr.io/lonard2/karsajobs-ui:latest
