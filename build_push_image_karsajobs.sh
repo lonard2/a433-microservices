e265512ea82cd7c082dd9f296db5783df5bf27a3
lonardsteven <113404058+lonardsteven@users.noreply.github.com>
2023-11-26 20:35:14 +0700

    Remove direct reference to personal access token

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -8,13 +8,9 @@
 # dan dibuatkan untuk versi latest. Image akan menggunakan username lonard2.
 docker build -t lonard2/karsajobs:latest
 
-# Membuat sebuah environment variable bernama PAT_BE (personal access token) yang berisikan token PAT dari GitHub yang telah diperbolehkan
-# (allowed) akses write & read package serta repository / repo. Perannya ialah untuk mendukung proses login Docker.
-export PAT_BE=
-
 # Login Docker ke container registry ghcr.io (GitHub Packages) dengan username GitHub pribadi dan password account berupa PAT_BE (environment variable)
 # pada line sebelumnya.
-echo $PAT_BE | docker login ghcr.io -u lonard2 --password-stdin
+echo ${PERSONAL_ACCESS_TOKEN} | docker login ghcr.io -u lonard2 --password-stdin
 
 # Dengan tag, nama image Docker dari nama local yakni lonard2/karsajobs:latest menjadi nama yang mengikuti standar GitHub Packages
 # (ghcr.io/NAMESPACE/package_name:tag) yakni ghcr.io/lonard2/karsajobs:latest
